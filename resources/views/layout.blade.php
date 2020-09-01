0775fde84ce26fa1a54039c4942fce905224100e
McPerry11 <mack.perry.co@gmail.com>
2020-09-01 11:14:10 +0800

    V2.3.4b - Server configuration fix 2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
   <base href="{{ url('/') . '/' }}">
   <meta charset="UTF-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="stylesheet" href="{{ asset('css/packages.css') }}?v={{ filemtime(asset('css/packages.css')) }}">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ filemtime(asset('css/app.css')) }}">
+  <link rel="stylesheet" href="{{ asset('css/packages.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <title>University Research Archiving System</title>
 </head>
