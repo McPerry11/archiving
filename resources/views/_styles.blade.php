caa383db6858c8493ea98d8ea61edf056f9c167f
McPerry11 <mack.perry.co@gmail.com>
2020-09-01 11:26:51 +0800

    V2.3.8 - Added shortcut icon

--- resources/views/_styles.blade.php
+++ resources/views/_styles.blade.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="{{ asset('img/logo/ue.png') }}">
 <link rel="stylesheet" href="{{ asset('css/packages.css') }}?v={{ filemtime(public_path('css/packages.css')) }}">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ filemtime(public_path('css/app.css')) }}">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
